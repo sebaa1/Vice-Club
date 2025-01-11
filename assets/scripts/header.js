0ce696c25c8704618ba4c8486208287eb589b976
seba <63276982+sebaa1@users.noreply.github.com>
2025-01-10 23:42:37 -0300

    this looks good

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -2,10 +2,10 @@ const header = document.getElementById('header');
 const logoContainer = document.getElementById('logo-container');
 const dropdownContent = document.getElementById('dropdown-content');
 function openMenu() {
-  dropdownContent.style.display = 'flex';  // Muestra el menú
+  dropdownContent.style.display = 'flex';
 }
 function closeMenu() {
-  dropdownContent.style.display = 'none';  // Oculta el menú
+  dropdownContent.style.display = 'none'; 
 }
 logoContainer.addEventListener('mouseenter', openMenu);
 header.addEventListener('mouseleave', closeMenu);
