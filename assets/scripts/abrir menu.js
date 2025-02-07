37ab0c59712caab353881cb6e93900d2fe14c680
seba <63276982+sebaa1@users.noreply.github.com>
2025-02-07 18:12:54 -0300

    New things COMING UPBRAH

--- assets/scripts/abrir menu.js
+++ assets/scripts/abrir menu.js
@@ -1,3 +1,13 @@
-document.querySelector('hamburger').addEventListener('click', function() {
-    document.querySelector('.menu-content').classList.toggle('show');
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.getElementById("hamburger");
+    const menuContent = document.querySelector(".menu-content");
+    hamburger.addEventListener("click", function (event) {
+        event.stopPropagation();
+        menuContent.classList.toggle("show");
+    });
+    document.addEventListener("click", function (event) {
+        if (!menuContent.contains(event.target) && !hamburger.contains(event.target)) {
+            menuContent.classList.remove("show");
+        }
+    });
 });
